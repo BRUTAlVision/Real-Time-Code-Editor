3e0b0219c2450b0a0e660750852ff923350aa7d4
Ujjwal Garg <74059664+BRUTAlVision@users.noreply.github.com>
2023-05-24 21:07:49 +0530

    final part

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,60 +1,88 @@
-import React, { useState } from 'react'
-import logo from '../images/logo.jpg'
-import { v4 as uuidV4 } from 'uuid'
-import toast from 'react-hot-toast'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const navigate = useNavigate();
+
     const [roomId, setRoomId] = useState('');
-    const [username, setusername] = useState('');
+    const [username, setUsername] = useState('');
     const createNewRoom = (e) => {
         e.preventDefault();
         const id = uuidV4();
         setRoomId(id);
-        toast.success('Create a new room ');
+        toast.success('Created a new room');
+    };
 
-    }
     const joinRoom = () => {
         if (!roomId || !username) {
-            toast.error('Room Id & Username is required')
+            toast.error('ROOM ID & username is required');
+            return;
         }
-        else {
 
-            //redirect 
-            navigate(`/editor/${roomId}`, {
-                state: {
-                    username
-                },
-            })
-        }
-    }
+        // Redirect
+        navigate(`/editor/${roomId}`, {
+            state: {
+                username,
+            },
+        });
+    };
+
     const handleInputEnter = (e) => {
         if (e.code === 'Enter') {
             joinRoom();
         }
     };
     return (
-        <>
-            <div className="homePageWrapper">
-                <div className="formWrapper">
-                    <img className='homePageLogo' src={logo} alt="logo" />
-                    <h4 className='mainlabel'>Paste invitation ROOM ID</h4>
-                    <div className="inputGroup">
-                        <input type="text" className='inputBox' placeholder='ROOM ID' value={roomId} onChange={(e) => setRoomId(e.target.value)}  onKeyUp={handleInputEnter} />
-                        <input type="text" className='inputBox' placeholder='USERNAME' value={username} onChange={(e) => setusername(e.target.value)}  onKeyUp={handleInputEnter}/>
-                        <button className='btn joinBtn' onClick={joinRoom}>Join</button>
-                        <span className='createInfo'>If you don't have invite then create &nbsp; <a onClick={createNewRoom} href="#" className='createNewBtn'>new room</a></span>
-                    </div>
+        <div className="homePageWrapper">
+            <div className="formWrapper">
+                <img
+                    className="homePageLogo"
+                    src="/code-sync.png"
+                    alt="code-sync-logo"
+                />
+                <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+                <div className="inputGroup">
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="ROOM ID"
+                        onChange={(e) => setRoomId(e.target.value)}
+                        value={roomId}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="USERNAME"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <button className="btn joinBtn" onClick={joinRoom}>
+                        Join
+                    </button>
+                    <span className="createInfo">
+                        If you don't have an invite then create &nbsp;
+                        <a
+                            onClick={createNewRoom}
+                            href=""
+                            className="createNewBtn"
+                        >
+                            new room
+                        </a>
+                    </span>
                 </div>
-                <footer>
-                    <h4>Built with ❤️ By Ujjwal Garg</h4>
-                </footer>
             </div>
+            <footer>
+                <h4>
+                    Built with 💛 &nbsp; by &nbsp;
+                    <span>Angry DEV</span>
+                </h4>
+            </footer>
+        </div>
+    );
+};
 
-
-        </>
-    )
-}
-
-export default Home
+export default Home;
